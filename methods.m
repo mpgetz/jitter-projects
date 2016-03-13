501beeb7418957073edf2fbcfffd03baf7266235
mpgetz <mattpgetz@gmail.com>
2016-03-13 13:22:35 -0400

    new raster generator

--- methods.m
+++ methods.m
@@ -1,6 +1,23 @@
 % collection of spike train analysis methods
-classdef s
+
+classdef st
     methods
         function [] = cch(n1, n2);
         end
+
+        function [] = raster(varargin)
+        % generates raster plot of input spike trains
+            args = varargin;
+
+            figure
+            hold on
+            for n=1:length(args)
+                arg = args(n);
+                for i=1:length(arg)
+                    line([arg(i) arg(i)], [n-1, n]);
+                end
+            end
+            hold off
+        end
     end
+end
