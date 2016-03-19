ea89f4cc96066bd40f21faa989b0c14240e8d3a4
mpgetz <mattpgetz@gmail.com>
2016-03-19 16:13:35 -0400

    edits to trainmethods

--- TrainMethods.m
+++ TrainMethods.m
@@ -2,24 +2,28 @@
 
 classdef TrainMethods
     methods
-        function [counts] = cch(self, n1, n2, disc, maxlag);
+        function [counts] = cch(self, n1, n2, bin, lag, maxlag);
         % creates CCH for spike trains n1, n2
-            counts = zeros(1, (2*maxlag)/disc);
-            lags = -maxlag:disc:maxlag;
+            counts = zeros(1, (2*maxlag)/lag);
+            lags = -maxlag:lag:maxlag;
 
             %n1 = round(n1, 3);
             %n2 = round(n2, 3);
             for l=1:length(lags)
                 lag = lags(l);
                 count = 0;
+                lower = (lag-0.5)*bin;
+                upper = (lag+0.5)*bin;
+
                 for s=1:length(n2)
-                    count = count + sum((n1+lag)==n2(s));
+                    count = count + length(intersect(find((n1+lower)<=n2(s)), find((n2(s)<(n1+upper)))));
                 end
                 counts(l) = count;
             end
 
             % plot counts
-            bar([-maxlag:disc:maxlag], counts);
+            bar(lags, counts);
+            xlim([-maxlag-0.5, maxlag+0.5]);
         end
 
         function [] = raster(varargin)
@@ -36,7 +40,8 @@ classdef TrainMethods
 
                 % truncate to a(n arbitrarily) manageable size
                 if l_a > 500
-                    arg = arg(0:500);
+                    arg = arg(1:500);
+                    l_a = 500;
                 end
 
                 for i=1:l_a
