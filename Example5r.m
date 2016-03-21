4ed72dd4dcd2754251339ac57651bf186a38f763
mpgetz <mattpgetz@gmail.com>
2016-03-21 14:12:37 -0400

    edits to real ex5

--- Example5r.m
+++ Example5r.m
@@ -1,6 +1,7 @@
 %%
-%Repeat Example5 situation but with Poisson train and phase matched to frate Hz sine wave
-%(+1 if [0, pi), -1 if [-pi, 2*pi)
+% Repeat Example5 situation but with Poisson train and phase matched to frate Hz sine wave
+% (+1 if [0, pi), -1 if [-pi, 2*pi)
+% msec discretization
 T = 100;%sec
 trials = 5000;
 num_jitter_trials = 500;
@@ -12,12 +13,13 @@ probs1 = zeros(1, trials);
 probstu = zeros(1, trials);
 probstd = zeros(1, trials);
 
-win = 0.005; %1/2 jitter window width
-bins = (0+win):.001:(1-win); 
+win = 0.006; %1/2 jitter window width
+bins = 0:.001:(T-.001); 
+interval = 0:.001:(2*win)-.001;
 
 % neuron data
 %frate = 5;
-m = 40;
+m = 10;
 
 % phase data
 freq = 120; 
@@ -36,18 +38,18 @@ for runs=1:trials
 
     %choose exactly m spikes from msec bins:
     n1 = randsample(bins, m);
-
-    dur1 = length(n1);
-    lengths(1,runs) = dur1;
+    dur1 = m;
 
     pref = rphase_stat(n1, freq);
-    prefs(1,runs) = pref;
 %    display(pref);
     jit_stats1 = zeros(1,num_jitter_trials);
 
     for t=1:num_jitter_trials
-        jit = n1 + ((2*win*rand(1,dur1))-win);
-        jit_stats1(1,t) = rphase_stat(jit, freq);
+        jit = n1 + ((datasample(interval ,dur1))-win);
+
+        %jit_stats1(1,t) = rphase_stat(jit, freq);
+        jit_stats1(1,t) = sum(sin(freq*2*pi*jit)>0);
+
     end
     
     %calculate p-value
@@ -57,7 +59,10 @@ for runs=1:trials
     %display(dn);
     ups1(1,runs) = up1;
     dns1(1,runs) = dn1;
-  
+
+    if mod(runs, 100) == 0
+        display(runs);
+    end
 
 end
 
