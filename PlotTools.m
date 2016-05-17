df75ea244ae5da8fafcd1a2991f21156c20fad5d
EM <mattpgetz@gmail.com>
2016-05-17 14:28:14 -0400

    fixed first method

--- PlotTools.m
+++ PlotTools.m
@@ -3,18 +3,17 @@ classdef PlotTools
     end
 
     methods
-        function [] = plot_wvs(self, wv_data, x, y)
-            figure
+        function [] = plot_wvs(self, wv_data, p, test2, x, y)
+            figure;
             %x a multiple of 8
             %if y > 4, newline
             for k=1:4; 
-                figure; 
                 for i=1:8; 
-                    subplot(8*x, y, i); 
-                    plot(wv_2(i, :, p(k))); 
+                    subplot(8*x, y, k+(y*(i-1))); 
+                    plot(wv_data(i, :, p(k))); 
                     ylim([min(min(test2(:,:,p(k)))), max(max(test2(:,:,p(k))))]); 
-                end; 
-            end;
+                end 
+            end
         end
     end
 end
